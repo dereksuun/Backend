9e005d36b7af24fca534367d322dde730438583d
Deryck <deryck.ferreira30@gmail.com>
2026-06-20 15:49:52 -0300

    fix: bind API to Render host

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import { env } from "./env.js";
 
 const app = createApp();
 
-app.listen(env.PORT, () => {
-  console.log(`Derycash backend running on http://localhost:${env.PORT}`);
+app.listen(env.PORT, "0.0.0.0", () => {
+  console.log(`Derycash backend running on port ${env.PORT}`);
 });
